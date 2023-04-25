73716c1484114eb09821c01a6a1f18457ac8e1eb
Rokayanasr <rokanasr60@gmail.com>
2023-04-24 19:17:10 -0700

    ${"Commit"}

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
  */
 
 int main(void)
-printf("size of char: %lu byte(s)/n", char);
-printf("size of int: %lu byte(s)/n", int);
-printf("size of long int: %lu byte(s)/n", long int);
-printf("size of long long int: %lu byte(s)/n", long long int);
-printf("size of float: %lu byte(s)/n", float);
+  printf("size of char: %lu byte(s)/n", sizeof(char));
+printf("size of int: %lu byte(s)/n", sizeof(int));
+printf("size of long int: %lu byte(s)/n", sizeof(long int));
+printf("size of long long int: %lu byte(s)/n", sizeof(long long int));
+printf("size of float: %lu byte(s)/n", sizeof(float));
 return (0);
