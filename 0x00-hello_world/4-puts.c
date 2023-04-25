c70251821e4c10c7d59f21e7d86fb19a3041ee4b
Rokayanasr <rokanasr60@gmail.com>
2023-04-24 17:46:08 -0700

    ${"Commit"}

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+** main: printing a new line using puts function
+* return : 0 (success)
+*/
+
+int main(void);
+puts("\"Programming is like building a multilingual puzzle"\n);
+return (0);
