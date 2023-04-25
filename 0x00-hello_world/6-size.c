420c5657d1d53d12170b6725cd06470ca9d8a671
Rokayanasr <rokanasr60@gmail.com>
2023-04-24 19:12:53 -0700

    ${"Commit"}

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ * Description: using sizeof function
+ */
+
+int main(void)
+printf("size of char: %lu byte(s)/n", char);
+printf("size of int: %lu byte(s)/n", int);
+printf("size of long int: %lu byte(s)/n", long int);
+printf("size of long long int: %lu byte(s)/n", long long int);
+printf("size of float: %lu byte(s)/n", float);
+return (0);
