4f6c3fb23d7810da380613bea40bc144dac6874f
Rokayanasr <rokanasr60@gmail.com>
2023-04-24 18:54:44 -0700

    ${"Commit"}

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
 * main - Entry point
 * Return: Always 0 (success)
-*Description: using printf to print new line
+* Description: using printf to print new line
 */
-int main(void);
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
