8293fe5f0f3f2f99be2f5c4fb2c8b7b86264803d
Rokayanasr <rokanasr60@gmail.com>
2023-04-29 07:24:56 -0700

    {$"COMMIT"}

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'Print alphabets lowercase'
+*Return: always 0
+*/
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++ )
+{
+
+putchar(ch);
+putchar('\n');
+}
+return (0);
+}
