e258d8ad7cb94756a91063e71ae108dc3d44f918
Rokayanasr <rokanasr60@gmail.com>
2023-04-28 18:22:35 -0700

    {$"COMMIT"}

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
-*Description: 'infinite loop checking'
-*Return: always: 0
+* main - causes an infinite loop
+* Return: 0
 */
 
 int main(void)
@@ -13,12 +12,14 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
+ 
 /**
 * while (i < 10)
 * {
 * putchar(i);
 * }
 */
+ 
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
