edc3800ae708f8b633c10420edb51380135308ad
Rokayanasr <rokanasr60@gmail.com>
2023-04-29 16:29:19 -0700

    {$"COMMIT"}

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'prints all possible combinations of single-digit numbers'
+*Return: always 0
+*/
+int main(void)
+{
+int n = 48;
+
+
+while (n <= 57)
+{
+putchar(n);
+putchar(44);
+putchar(32);
+
+n++;
+}
+
+putchar('\n');
+return (0);
+}
