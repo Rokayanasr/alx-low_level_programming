604d2004bb5c165b728c4b51a34d74b5752325c5
Rokayanasr <rokanasr60@gmail.com>
2023-04-28 17:06:33 -0700

    {$"COMMIT"}

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main - entry point
-*Description: checking the n is positive or negative
-*Return always 0
+*main - Entry point
+*Description: 'checking the n is positive or negative'
+*Return: always: 0
 */
 int main(void)
 {
