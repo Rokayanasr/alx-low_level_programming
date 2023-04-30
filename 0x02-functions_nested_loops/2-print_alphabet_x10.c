ef593742f7ce7b02728cd3325895b04dc457e8cd
Rokayanasr <rokanasr60@gmail.com>
2023-04-30 13:20:44 -0700

    {$"COMMIT"}

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,19 @@
 
 /**
 * main - Entry point
-* print_alphabet_x10 - make alphabets 10 time
-* Return: always 0
+* print_alphabet_x10 - Prints alphabets 10 time.
+* Return: always 0 (success)
 */
 
 void print_alphabet_x10(void)
 {
 int a;
 int b;
-for (a = 1; b <= 10; b++)
+for (b = 1; b <= 10; b++)
 {
 for (a = 97; a <= 122; a++)
 {
-_putchar(97);
+_putchar(a);
 }
 _putchar('\n');
 }
