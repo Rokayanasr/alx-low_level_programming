fd5e6abad8be2373f7418e84818b65967989529a
Rokayanasr <rokanasr60@gmail.com>
2023-04-30 10:02:29 -0700

    {$"COMMIT"}

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,22 @@
-#include <stdio.h>
-int main(void)
+#include "main.h"
 
+/**
+*main - Entry point
+*Description: 'prints _putchar'
+*Return: always 0
+*/
+
+int main(void)
 {
-char c = '_putchar';
-printf("%c\n", c);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+ _putchar('\n');
 return (0);
 }
+
