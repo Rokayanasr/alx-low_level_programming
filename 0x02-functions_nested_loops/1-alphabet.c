5cf974fe82121e686d5edb219d700ac77be4ac4b
Rokayanasr <rokanasr60@gmail.com>
2023-04-30 10:24:22 -0700

    {$"COMMIT"}

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - Entry point
+*Description: 'prints the alphabet, in lowercase'
+*Return: always 0
+*/
+
+int main(void)
+{
+int a;
+for (a = 97; a <= 122; a++)
+{
+_putchar(97);
+}
+_putchar('\n');
+return (0);
+}
