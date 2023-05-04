39aa26823f7584139e7d83c418edae6005967be5
Rokayanasr <rokanasr60@gmail.com>
2023-05-03 18:40:49 -0700

    {$"COMMIT"}

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,17 +6,17 @@
 
 int main(void)
 {
-    char password[PASSWORD_LEN + 1] = {0};
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
-    const size_t charset_size = sizeof(charset) - 1;
+char password[PASSWORD_LEN + 1] = {0};
+const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+const size_t charset_size = sizeof(charset) - 1;
 
-    srand(time(NULL));
+srand(time(NULL));
 
-    for (int i = 0; i < PASSWORD_LEN; i++) {
-      password[i] = charset[rand() % charset_size];
-    }
+for (int i = 0; i < PASSWORD_LEN; i++) {
+password[i] = charset[rand() % charset_size];
+}
 
-    printf("%s\n", password);
+printf("%s\n", password);
 
-    return 0;
+return (0);
 }
